24a29e5fa38b81995e997577ad786cd343af1950
cefalo-salahuddin <salahuddin@cefalo.com>
2016-10-03 11:48:49 +0600

    laravel tasklist application update with adding, deleting and listing tasks

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,47 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | This file is where you may define all of the routes that are handled
+  | by your application. Just tell Laravel the URIs it should respond
+  | to using a Closure or controller method. Build something great!
+  |
+ */
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+     $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+            ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
 });
